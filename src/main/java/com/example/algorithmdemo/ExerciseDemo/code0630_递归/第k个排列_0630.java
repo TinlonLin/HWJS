c60ca2b998022c66f87dc1a470c4e8174eb9cd96
TinloLin <tilolin@qq.com>
2022-06-30 07:41:16 +0800

    玩牌高手 第k个排列 素数之积

--- src/main/java/com/example/algorithmdemo/ExerciseDemo/code0630_递归/第k个排列_0630.java
+++ src/main/java/com/example/algorithmdemo/ExerciseDemo/code0630_递归/第k个排列_0630.java
@@ -1,14 +1,55 @@
 package com.example.algorithmdemo.ExerciseDemo.code0630_递归;
 
+import java.util.Scanner;
+
 /**
  * @author: TinlonLin
  * @email: tilolin@qq.com
  * @classname: 第k个排列_0630
- * @desc: Todo
+ * @desc: 第k个排列
+ * https://blog.csdn.net/weixin_44219664/article/details/124089294
+ *
+ * 给定参数n，从1到n会有n个整数 1,2,3...n
+ * 这n个数字共有n!种排序 按大小顺序升序列出所有排序情况，并一一标记
+ * 当n=3时，所有排列如下
+ * 123 132 213 231 312 321
+ *
+ * 给定n和k 返回第k个排列
+ *
+ * 输入描述：
+ * 第一行为 n 1~9
+ * 第二行为 k 1~n!
+ *
+ * 输出描述：
+ * 输出排列第k位置的数字
+ *
+示例一：
+输入
+3
+3
+输出
+213
+
+示例二:
+输入
+2
+2
+输出
+21
+
  *
  * @date: 2022/6/21 11:19 下午
  * @version: V-1.0
  */
 public class 第k个排列_0630 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+        int k = Integer.parseInt(sc.nextLine());
+        getNokNum(n, k);
+    }
 
+    private static void getNokNum(int n, int k) {
+
+    }
 }
--- src/main/java/com/example/algorithmdemo/ExerciseDemo/code0630_递归/素数之积_0630.java
+++ src/main/java/com/example/algorithmdemo/ExerciseDemo/code0630_递归/素数之积_0630.java
@@ -1,14 +1,59 @@
 package com.example.algorithmdemo.ExerciseDemo.code0630_递归;
 
+import java.util.Scanner;
+
 /**
  * @author: TinlonLin
  * @email: tilolin@qq.com
  * @classname: 素数之积_0630
- * @desc: Todo
- *
+ * @desc: 素数之积
+ * https://blog.csdn.net/qq_34465338/article/details/125028089
+ * 给定一个 32 位正整数，对其进行因数分解，分解成两个素数的乘积，如果不能分解输出 -1,-1 。
+ * 输入
+ * 15
+ * 输出
+ * 3,5
+ * 输入：
+ * 27
+ * 输出：
+ * -1,-1
  * @date: 2022/6/21 11:20 下午
  * @version: V-1.0
  */
 public class 素数之积_0630 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = Integer.parseInt(sc.nextLine());
+        getTwoPrime(num);
+    }
 
+    private static void getTwoPrime(int num) {
+        //满足条件的两个数，都初始为-1
+        int a = -1;
+        int b = -1;
+        if (num > 3) {
+            for (int i = 2; i < num; i++) {
+                for (int j = i+1; j < num; j++) {
+                    if (isPrime(i) && isPrime(j) && i*j ==num) {
+                        a = i;
+                        b = j;
+                    }
+                }
+            }
+        }
+        System.out.println(a + "," + b);
+    }
+
+    //判断是否为素数
+    private static boolean isPrime(int n) {
+        if (n == 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(n) ; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
