932b0f7999b88ef4ff6c81fbc0fdb34608cc64ef
tinlonlin <tilolin@qq.com>
2022-07-12 22:34:13 +0800

    德州扑克 社交距离

--- src/main/java/com/example/algorithmdemo/ExerciseDemo/code0713_哈希表/德州扑克_0713.java
+++ src/main/java/com/example/algorithmdemo/ExerciseDemo/code0713_哈希表/德州扑克_0713.java
@@ -1,14 +1,141 @@
 package com.example.algorithmdemo.ExerciseDemo.code0713_哈希表;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
 /**
  * @author: TinlonLin
  * @email: tilolin@qq.com
  * @classname: 德州扑克_0713
- * @desc: Todo
- *
+ * @desc: 德州扑克
+ * https://blog.csdn.net/vsky_ma/article/details/83020594
+ * https://zh.wikipedia.org/zh-hans/%E6%92%B2%E5%85%8B%E7%89%8C%E5%9E%8B
+五张牌，每张牌由牌大小和花色组成，牌大小2~10、J、Q、K、A，牌花色为红桃、黑桃、梅花、方块四种花色之一。 判断牌型:
+
+牌型1，同花顺：同一花色的顺子，如红桃2红桃3红桃4红桃5红桃6。
+牌型2，四条：四张相同数字 + 单张，如红桃A黑桃A梅花A方块A + 黑桃K。
+牌型3，葫芦：三张相同数字 + 一对，如红桃5黑桃5梅花5 + 方块9梅花9。
+牌型4，同花：同一花色，如方块3方块7方块10方块J方块Q。
+牌型5，顺子：花色不一样的顺子，如红桃2黑桃3红桃4红桃5方块6。
+牌型6，三条：三张相同 + 两张单。
+牌型7，其他。
+
+说明：
+1）五张牌里不会出现牌大小和花色完全相同的牌。
+2）前面的牌型比后面的牌型大，如同花顺比四条大，依次类推。
+
+输入描述:
+输入由5行组成
+每行为一张牌大小和花色，牌大小为2~10、J、Q、K、A，花色分别用字符H、S、C、D表示红桃、黑桃、梅花、方块。
+
+输出描述:
+输出牌型序号，5张牌符合多种牌型时，取最大的牌型序号输出
+
+示例1
+输入：
+2 H
+3 C
+6 S
+5 S
+4 S
+
+输出：
+5
+
+A H
+2 S
+3 S
+4 S
+5 D
+
+10 H
+J H
+Q H
+K H
+A H
+5
  * @date: 2022/6/21 11:30 下午
  * @version: V-1.0
  */
 public class 德州扑克_0713 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] m = new int[5];
+        String[] n = new String[5];
+        for (int i = 0; i < 5; i++) {
+            String[] line = sc.nextLine().split("\\s+");
+            switch (line[0]) {
+                case "J":
+                    m[i] = 11;
+                    break;
+                case "Q":
+                    m[i] = 12;
+                    break;
+                case "K":
+                    m[i] = 13;
+                    break;
+                case "A":
+                    m[i] = 1;
+                    break;
+                default:
+                    m[i] = Integer.parseInt(line[0]);
+                    break;
+            }
+            n[i] = line[1];
+        }
+        ArrayList<Integer> list = new ArrayList<>();
+        boolean errflag = false;
+        for (int i = 0; i < n.length; i++) {
+            if (!"HSCD".contains(n[i])) {
+                System.out.println(7);
+                errflag = true;
+            }
+        }
+        Arrays.sort(m);
+        boolean nflag = false;
+        if (n[0].equals(n[1]) && n[1].equals(n[2]) && n[2].equals(n[3]) && n[3].equals(n[4])) {
+            nflag = true;
+            list.add(4);
+        }
 
+        int lcnt = 1;
+        for (int i = 0; i < m.length - 1; i++) {
+            if (m[i + 1] - m[i] == 1) {
+                lcnt++;
+            }
+        }
+        int e1cnt = 1;
+        int e2cnt = 1;
+        iw:
+        for (int i = 0; i < m.length - 1; i++) {
+            if (m[i + 1] == m[i]) {
+                e1cnt++;
+            } else if (m[i + 1] != m[i]) {
+                for (int j = i + 1; j < m.length - 1; j++) {
+                    if (m[j + 1] == m[j]) {
+                        e2cnt++;
+                    }
+                }
+                break iw;
+            }
+        }
+        if (lcnt == 5 && nflag) {
+            list.add(1);
+        } else if ((e1cnt == 4 || e2cnt == 4) && !nflag) {
+            list.add(2);
+        } else if ((e1cnt == 3 && e2cnt == 2) || ((e1cnt == 2 && e2cnt == 3)) && !nflag) {
+            list.add(3);
+        } else if (lcnt == 5 && !nflag) {
+            list.add(5);
+        } else if ((e1cnt == 3 && e2cnt == 1) || ((e1cnt == 1 && e2cnt == 3)) && !nflag) {
+            list.add(6);
+        } else {
+            list.add(7);
+        }
+        Collections.sort(list);
+        System.out.println(list.get(0));
+
+    }
 }
--- src/main/java/com/example/algorithmdemo/ExerciseDemo/code0713_哈希表/社交距离_0713.java
+++ src/main/java/com/example/algorithmdemo/ExerciseDemo/code0713_哈希表/社交距离_0713.java
@@ -1,16 +1,122 @@
 package com.example.algorithmdemo.ExerciseDemo.code0713_哈希表;
 
+import java.util.Scanner;
+import java.util.TreeSet;
+
 /**
  * @author: TinlonLin
  * @email: tilolin@qq.com
  * @classname: 社交距离_0713
- * @desc: Todo
+ * @desc: 社交距离
  * https://blog.csdn.net/weixin_44052055/article/details/124040054
  *
+题目描述：
+疫情期间需要大家保证一定的社交距离，公司组织开交流会议。
+
+座位一排共 N 个座位，编号分别为 [0, N - 1] , 要求员工一个接着一个进入会议室，并且可以在任何时候离开会议室。
+
+满足：
+
+每当一个员工进入时，需要坐到最大社交距离（最大化自己和其他人的距离的座位）；
+如果有多个这样的座位，则坐到 索引最小 的那个座位。
+输入描述：
+会议室座位总数 seatNum 。(1 <= seatNum <= 500)
+员工的进出顺序 seatOrLeave 数组，元素值为 1，表示进场；元素值为负数，表示出场（特殊：位置 0 的员工不会离开）。
+例如 - 4 表示坐在位置 4 的员工离开（保证有员工坐在该座位上）
+输出描述：
+最后进来员工，他会坐在第几个位置，如果位置已满，则输出 - 1 。
+
+示例 1：
+输入
+10
+[1,1,1,1,-4,1]
+1
+2
+输出
+5
+1
+说明
+seat -> 0 , 空在任何位置都行，但是要给他安排索引最小的位置，也就是座位 0
+seat -> 9 , 要和旁边的人距离最远，也就是座位 9
+seat -> 4 , 要和旁边的人距离最远，应该坐到中间，也就是座位 4
+seat -> 2 , 员工最后坐在 2 号座位上
+leave [4] , 4 号座位的员工离开
+seat -> 5 , 员工最后坐在 5 号座位上
+解题思路
+这道题坐的位置是满足最大距离下的最小索引，可以使用TreeSet有序去重的集合来存放已经坐过的位置，
+第一个进来的，只能坐到位置0，最后一个进来的只能坐到最后一个位置，
+中间进来的就需要找，离坐下的最远距离的最小索引位置，需要遍历已经坐下的位置，找到中间索引最小的最大距离。
 
  *
  * @date: 2022/6/21 11:30 下午
  * @version: V-1.0
  */
 public class 社交距离_0713 {
+        public static void main(String[] args) {
+            Scanner in = new Scanner(System.in);
+            int seatNum = in.nextInt();
+            in.nextLine();
+            String seat = in.nextLine();
+            String[] c = seat.substring(1, seat.length() - 1).split(",");
+            int[] seatOrLeave = new int[c.length];
+            for (int i = 0; i < c.length; i++) {
+                seatOrLeave[i] = Integer.parseInt(c[i]);
+            }
+            int ans = seatDistance(seatNum, seatOrLeave);
+            System.out.print(ans);
+        }
+        public static int seatDistance(int seatNum, int[] seatOrLeave) {
+            // 使用TreeSet有序集合记录被坐过的座位
+            TreeSet<Integer> seatedNums = new TreeSet<>();
+            for (int i = 0; i < seatOrLeave.length; i++) {
+                int op = seatOrLeave[i];
+                if (op > 0) {
+                    // 如果是第一个坐
+                    if (seatedNums.size() == 0) {
+                        // 如果只有一个位置
+                        if (i == seatOrLeave.length - 1) {
+                            return 0;
+                        }
+                        seatedNums.add(0);
+                    } else if (seatedNums.size() == 1) {
+                        // 第二个人进来，坐在最右边
+                        seatedNums.add(seatNum - 1);
+                        // 如果只有两个位置
+                        if (i == seatOrLeave.length - 1) {
+                            return seatNum - 1;
+                        }
+                    } else if (seatedNums.size() > 1 && seatedNums.size() < seatNum) {
+                        // 坐到中间的位置
+                        int[] ints = new int[seatedNums.size()];
+                        int count = 0;
+                        // 将已经坐过的位置存入到数组中
+                        for (Integer seatedNum : seatedNums) {
+                            ints[count++] = seatedNum;
+                        }
+                        int maxLen = 0;
+                        int start = 0;
+                        // 计算最远距离
+                        for (int j = 0; j < ints.length - 1; j++) {
+                            int len = ints[j + 1] - ints[j];
+                            if (len / 2 > maxLen) {
+                                maxLen = len / 2;
+                                start = ints[j];
+                            }
+                        }
+                        // 将对应的起始位置加上最远距离加入seatedNums
+                        seatedNums.add(start + maxLen);
+                        if (i == seatOrLeave.length - 1) {
+                            return start + maxLen;
+                        }
+                    } else {
+                        // 位置坐满
+                        return -1;
+                    }
+                } else {
+                    // 如果是负数，则将该座位移出
+                    seatedNums.remove(-op);
+                }
+            }
+            return 0;
+        }
 }
