9e3d5270fe0599d6dace4dc267265db194d3016d
tinlonlin <tilolin@qq.com>
2022-07-15 01:33:02 +0800

    用户调度问题 用连续自然数之和来表达整数

--- src/main/java/com/example/algorithmdemo/ExerciseDemo/code0715_循环/用户调度问题_0715.java
+++ src/main/java/com/example/algorithmdemo/ExerciseDemo/code0715_循环/用户调度问题_0715.java
@@ -1,5 +1,12 @@
 package com.example.algorithmdemo.ExerciseDemo.code0715_循环;
 
+import org.w3c.dom.ls.LSInput;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
 /**
  * @author: TinlonLin
  * @email: tilolin@qq.com
@@ -7,11 +14,77 @@ package com.example.algorithmdemo.ExerciseDemo.code0715_循环;
  * @desc: 用户调度问题
  * https://blog.csdn.net/qq_38584967/article/details/124867407
  *
-
+【用户调度问题】在通信系统中，一个常见的问题是对用户进行不同策略的调度，
+会得到不同的系统消耗和性能。
+假设当前有n个待串行调度用户，每个用户可以使用A/B/C三种不同的调度策略，
+不同的策略会消耗不同的系统资源。请你根据如下规则进行用户调度，并返回总的
+消耗资源数。
+规则：
+1. 相邻的用户不能使用相同的调度策略，例如，第1个用户使用了A策略，
+则第2个用户只能使用B或者C策略。
+2. 对单个用户而言，不同的调度策略对系统资源的消耗可以归一化后抽象为数值。
+例如，某用户分别使用A/B/C策略的系统消耗分别为15/8/17。
+3. 每个用户依次选择当前所能选择的对系统资源消耗最少的策略（局部最优），
+如果有多个满足要求的策略，选最后一个。
+输入描述：
+第一行表示用户个数n 接下来每一行表示一个用户分别使用三个策略的系统消耗 resA resB resC
+输出描述：
+最优策略组合下的总的系统资源消耗数
+所有策略对系统的资源消耗均为正整数，n < 1000
+实例：
+输入：
+3
+15 8 17
+12 20 9
+11 7 5
+输出：
+24
  *
  * @date: 2022/6/21 11:32 下午
  * @version: V-1.0
  */
 public class 用户调度问题_0715 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+        //存入二维数组中
+        int[][] allResArr = new int[n][3];
+        for (int i = 0; i < n; i++) {
+            String[] strArr = sc.nextLine().split(" ");
+            for (int j = 0; j < 3; j++) {
+                allResArr[i][j] = Integer.parseInt(strArr[j]);
+            }
+        }
+        dispatch(allResArr);
+    }
 
+    private static int totalCount = Integer.MAX_VALUE;
+
+    private static void dispatch(int[][] allResArr) {
+        //路径
+        LinkedList<Integer> track = new LinkedList<>();
+        dfs(track, 0, allResArr, -1);
+        System.out.println(totalCount);
+    }
+
+    private static void dfs(LinkedList<Integer> track, int n, int[][] allResArr, int preChoice) {
+        //判断结束
+        if (n == allResArr.length) {
+            totalCount = Math.min(totalCount, track.stream().mapToInt(value -> (int) value).sum());
+            return;
+        }
+        //做选择
+        for (int i = 0; i < allResArr[0].length; i++) {
+            //跳过上一个策略
+            if (i == preChoice) {
+                continue;
+            }
+            //选择
+            track.addLast(allResArr[n][i]);
+            //递归
+            dfs(track, n + 1, allResArr, i);
+            //回溯
+            track.removeLast();
+        }
+    }
 }
--- src/main/java/com/example/algorithmdemo/ExerciseDemo/code0715_循环/用连续自然数之和来表达整数_0715.java
+++ src/main/java/com/example/algorithmdemo/ExerciseDemo/code0715_循环/用连续自然数之和来表达整数_0715.java
@@ -1,5 +1,10 @@
 package com.example.algorithmdemo.ExerciseDemo.code0715_循环;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+
 /**
  * @author: TinlonLin
  * @email: tilolin@qq.com
@@ -7,11 +12,82 @@ package com.example.algorithmdemo.ExerciseDemo.code0715_循环;
  * @desc: 用连续自然数之和来表达整数
  * https://blog.csdn.net/weixin_44219664/article/details/123884052
  *
+一个整数可以由连续的自然数之和来表示。给定一个整数，计算该整数有几种连续自然数之和的表达式，且打印出每种表达式。
+
+输入描述
+一个目标整数 t，1 <= t <= 1000
+
+输出描述
+1、该整数的所有表达式和表达式的个数。如果有多种表达式，自然数个数最少的表达式优先输出
+2、每个表达式中按自然数递增输出
+
+具体的格式参见样例
+在每个测试数据结束时，输出一行"Result:X"，其中 X 是最终的表达式个数
+
+示例 1
+输入
+9
+输出
+9=9
+9=4+5
+9=2+3+4
+Result:3
+
+示例 2
+输入
+10
+输出
+10=10
+10=1+2+3+4
+Result:2
 
  *
  * @date: 2022/6/21 11:32 下午
  * @version: V-1.0
  */
 public class 用连续自然数之和来表达整数_0715 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = Integer.parseInt(sc.nextLine());
+        getAllExpressionAndCount(t);
+    }
 
+    private static void getAllExpressionAndCount(int t) {
+        List<List<Integer>> resultList = new ArrayList<>();
+        //每个数本身就符合要求
+        List<Integer> tmpList = new ArrayList<>();
+        tmpList.add(t);
+        resultList.add(tmpList);
+        //双重循环，寻找是否存在1~t-1开头的连续数和满足条件
+        for (int i = 1; i < t; i++) {
+            List<Integer> singleList = new ArrayList<>();
+            int sum = 0;
+            for (int j = i; sum < t; j++) {
+                sum += j;
+                singleList.add(j);
+                //满足条件才加入结果中
+                if (sum == t) {
+                    resultList.add(singleList);
+                    break;
+                }
+            }
+        }
+        //按照长度升序排序
+        resultList.sort(new Comparator<List<Integer>>() {
+            @Override
+            public int compare(List<Integer> o1, List<Integer> o2) {
+                return o1.size() - o2.size();
+            }
+        });
+        //拼接输出结果
+        for (List<Integer> list : resultList) {
+            StringBuilder builder = new StringBuilder();
+            builder.append(t).append("=");
+            for (Integer integer : list) {
+                builder.append(integer).append("+");
+            }
+            System.out.println(builder.substring(0, builder.length() - 1).toString());
+        }
+        System.out.println("Result:" + resultList.size());
+    }
 }
