adfa462184e9746adeadeb47aa2cd531288e8539
tinlonlin <tilolin@qq.com>
2022-07-16 12:44:48 +0800

    计算运行时间和占用内存

--- src/main/java/com/example/algorithmdemo/utils/utils.java
+++ src/main/java/com/example/algorithmdemo/utils/utils.java
@@ -1,5 +1,9 @@
 package com.example.algorithmdemo.utils;
 
+import jdk.internal.org.objectweb.asm.tree.MultiANewArrayInsnNode;
+
+import java.text.DecimalFormat;
+
 /**
  * @author: TinlonLin
  * @email: tilolin@qq.com
@@ -9,5 +13,30 @@ package com.example.algorithmdemo.utils;
  * @version: V-1.0
  */
 public class utils {
+    public static void main(String[] args) {
+        Runtime runtime = Runtime.getRuntime();
+        runtime.gc();
+        long startMemory = runtime.freeMemory();
+        long startTime = System.currentTimeMillis();
+        //待运行方法
+        System.out.println((1+2));
+        long endTime = System.currentTimeMillis();
+        System.out.println("运行时间：" + (endTime - startTime) + "ms");
+        long endMemory = runtime.freeMemory();
+        DecimalFormat df = new DecimalFormat("###.0");
+        System.out.println("占用内存：" + df.format((startMemory - endMemory) / (1024.0 * 1024.0)) + "MB");
+    }
 
+    public static void getRumTimeAndMemory() {
+        Runtime runtime = Runtime.getRuntime();
+        runtime.gc();
+        long startMemory = runtime.freeMemory();
+        long startTime = System.currentTimeMillis();
+        //待运行方法
+        long endTime = System.currentTimeMillis();
+        System.out.println("运行时间：" + (endTime - startTime) + "ms");
+        long endMemory = runtime.freeMemory();
+        DecimalFormat df = new DecimalFormat("###.0");
+        System.out.println("占用内存：" + df.format((startMemory - endMemory) / (1024.0 * 1024.0)) + "MB");
+    }
 }
